b95638189a368d78bea123ff3530bc0b5c11fa6f
stnhhs2003-creator <stnhhs2003@rwm.kh.edu.tw>
2026-08-19 11:22:47 +0800

    Deployed by GitFolder Express

---
+++ dev_server.py
@@ -0,0 +1,29 @@
+import http.server
+import socketserver
+import os
+import urllib.parse
+
+PORT = 8000
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class CleanUrlHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_GET(self):
+        parsed = urllib.parse.urlparse(self.path)
+        clean_path = parsed.path
+        local_path = self.translate_path(clean_path)
+        
+        if not os.path.exists(local_path) and os.path.exists(local_path + '.html'):
+            if parsed.query:
+                self.path = clean_path + '.html?' + parsed.query
+            else:
+                self.path = clean_path + '.html'
+        return super().do_GET()
+
+if __name__ == '__main__':
+    socketserver.TCPServer.allow_reuse_address = True
+    with socketserver.TCPServer(("", PORT), CleanUrlHandler) as httpd:
+        print(f"Server running at http://localhost:{PORT}")
+        httpd.serve_forever()
